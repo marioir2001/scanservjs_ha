354d9f39c46368ad11681b28801b9379d73cd722
marioir2001 <Marioir2001@gmail.com>
2026-07-30 10:47:38 +0200

    Delete Scanservjs Dateien/config directory

--- Scanservjs Dateien/config/config/paths.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  PDF_TARGET: "/targets/pdf",
-  IMAGE_TARGET: "/targets/image",
-  MEMBER_APPLICATION_TARGET: "/targets/pdf/Mitgliedsantraege",
-  SPLIT_TARGET: "/var/lib/scanservjs/output/split",
-  PAPERLESS_TARGET: "/targets/paperless",
-};
--- Scanservjs Dateien/config/config/targets.js
+++
@@ -1,9 +0,0 @@
-﻿"use strict";
-
-module.exports = {
-  pdf: "/targets/pdf",
-  image: "/targets/image",
-  memberApplication: "/targets/pdf/Mitgliedsantraege",
-  invoice: "/targets/pdf/Rechnungen",
-  paperless: "/targets/paperless",
-};
