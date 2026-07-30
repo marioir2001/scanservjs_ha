ff49d3cb8ac470722c014c66b4999973e91c85c8
marioir2001 <Marioir2001@gmail.com>
2026-07-30 12:06:41 +0200

    Update paths.js

--- Scanservjs Dateien/etc/scanservjs/config/paths.js
+++ Scanservjs Dateien/etc/scanservjs/config/paths.js
@@ -1,9 +1,5 @@
 "use strict";
 
 module.exports = {
-  PDF_TARGET: "/targets/pdf",
-  IMAGE_TARGET: "/targets/image",
-  MEMBER_APPLICATION_TARGET: "/targets/pdf/Mitgliedsantraege",
   SPLIT_TARGET: "/var/lib/scanservjs/output/split",
-  PAPERLESS_TARGET: "/targets/paperless",
 };
